9707ea699c69c10a907e0bbeeb784ecbb2f963e3
Sergey <jaxi@atma.company>
2022-04-20 15:41:34 +0500

    Изменил верстку основной страницы

--- src/React/MainPage.js
+++ src/React/MainPage.js
@@ -46,56 +46,22 @@ const MainPage = () => {
                     </div>
                 </navigation>
                 <div className={"f-row-type max_width"}>
-                    <div>
-                        <section style={{ width: '50%' }}>
+                    <div className={"f-row-center-wrap"}>
+                        <section style={{ width: '60%' }}>
                             <div className={"f-cards"}>
                                 <div>
-                                    <p className={"f-cards-hashtag"}>#Предложить идею</p>
+                                    <p className={"f-cards-hashtag"}>#Вопрос</p>
                                     <div className={"f-cards-card-wrap"}>
                                         <div className={"f-cards-inner"}>
                                             <div className={"f-cards-avatar"}>
-                                                <img className={"f-cards-image"} src={"/i/avatar.png"}/>
-                                                <span className={"f-cards-text"}>Константин Константинопольский</span>
-                                            </div>
-                                            <div className={"f-cards-div-wrap-text"}>
-                                                <span className={"f-cards-content-text"}>
-                                                    Крайне не удобно работать
-                                                    с множественным
-                                                    выделением на карте, так как
-                                                    Ctrl и Shift + click
-                                                    не работают
-                                                </span>
-                                            </div>
-                                            <div className={"f-cards-under-block"}>
-                                                <div>
-                                                    <span className={"f-cards-under-block-comment"}>5 комментариев</span>
+                                                <div className={"f-cards-row-wrap"}>
+                                                    <img className={"f-cards-image"} src={"/i/avatar.png"}/>
+                                                    <div className={"f-cards-wrap-text"}>
+                                                        <span className={"f-cards-text"}>Константин Константинопольский</span>
+                                                        <span className={"f-cards-text-bottom"}>Генератор идей</span>
+                                                    </div>
                                                 </div>
-                                                <div>
-                                                    <a className={"f-cards-under-block-like"} href={"#"}>
-                                                        <img src={"/i/fire.svg"} />
-                                                        <span className={"f-cards-under-block-like-text"}>24</span>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        {/*<div style={{ height: 'inherit', width: 200, backgroundColor: 'black'}}>*/}
-                                        {/*    <img className={"f-cards-border-image"} alt={"image"} src={"/i/border-image.png"} />*/}
-                                        {/*</div>*/}
-                                    </div>
-                                </div>
-                                {/*<div></div>*/}
-                                {/*<div></div>*/}
-                            </div>
-                        </section>
-                        <section style={{ width: '70%' }}>
-                            <div className={"f-cards"}>
-                                <div>
-                                    <p className={"f-cards-hashtag"}>#Предложить идею</p>
-                                    <div className={"f-cards-card-wrap"}>
-                                        <div className={"f-cards-inner"}>
-                                            <div className={"f-cards-avatar"}>
-                                                <img className={"f-cards-image"} src={"/i/avatar.png"}/>
-                                                <span className={"f-cards-text"}>Константин Константинопольский</span>
+                                                    <p className={"f-cards-type"}>Рассмотрено</p>
                                             </div>
                                             <div className={"f-cards-div-wrap-text"}>
                                                 <span className={"f-cards-content-text"}>
@@ -115,19 +81,25 @@ const MainPage = () => {
                                             </div>
                                             <div className={"f-cards-under-block"}>
                                                 <div>
-                                                    <span className={"f-cards-under-block-comment"}>2 комментариев</span>
+                                                    <span className={"f-cards-under-block-comment"}>5 комментариев</span>
                                                 </div>
                                                 <div>
                                                     <a className={"f-cards-under-block-like"} href={"#"}>
-                                                        <img src={"/i/shit.svg"} />
-                                                        <span className={"f-cards-under-block-like-text"}>12</span>
+                                                        <img src={"/i/fire.svg"} />
+                                                        <span className={"f-cards-under-block-like-text"}>24</span>
+                                                    </a>
+                                                </div>
+                                                <div>
+                                                    <a className={"f-cards-under-block-like"} href={"#"}>
+                                                        <img src={"/i/fire.svg"} />
+                                                        <span className={"f-cards-under-block-like-text"}>Не нравится</span>
                                                     </a>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className={"f-cards-border-image-wrap"}>
-                                            <img className={"f-cards-border-image"} alt={"image"} src={"/i/border-image.png"} />
-                                        </div>
+                                        {/*<div style={{ height: 'inherit', width: 200, backgroundColor: 'black'}}>*/}
+                                        {/*    <img className={"f-cards-border-image"} alt={"image"} src={"/i/border-image.png"} />*/}
+                                        {/*</div>*/}
                                     </div>
                                 </div>
                                 {/*<div></div>*/}
@@ -137,19 +109,15 @@ const MainPage = () => {
                     </div>
                     <section style={{ width: '20%' }}>
                         <div className={"f-side-block"}>
-                            <div className={"f-side-panel-count"}>
-                                <div className={"f-side-panel-wrapper"}>
-                                    <span className={"f-side-panel-count-text"}>56</span>
-                                    <span className={"f-side-panel-count-subtext"}>Готовы</span>
-                                </div>
-                                <div className={"f-side-panel-wrapper"}>
-                                    <span className={"f-side-panel-count-text"}>34</span>
-                                    <span className={"f-side-panel-count-subtext"}>В работе</span>
-                                </div>
+                            <div className={"f-side-panel-wrap"} style={{ marginTop: 100 }}>
+                                <a className={"f-side-panel-button-section"} href={"#"}>В работе <span className={"f-side-panel-count-subtext"}>56</span></a>
+                                <a className={"f-side-panel-button-section f-selected"} href={"#"}>Запланировано <span className={"f-side-panel-count-subtext f-block-select"}>72</span></a>
+                                <a className={"f-side-panel-button-section"} href={"#"}>Рассмотрено <span className={"f-side-panel-count-subtext"}>2356</span></a>
+                                <a className={"f-side-panel-button-section"} href={"#"}>Завершено <span className={"f-side-panel-count-subtext"}>532</span></a>
                             </div>
                             <div className={"f-side-panel-wrap"}>
-                                <a className={"f-side-panel-button"} href={"#"}>#Задать вопрос</a>
-                                <a className={"f-side-panel-button"} href={"#"}>#Предложить идею</a>
+                                <a className={"f-side-panel-button"} href={"#"}>#Вопрос</a>
+                                <a className={"f-side-panel-button"} href={"#"}>#Идея</a>
                                 <a className={"f-side-panel-button"} href={"#"}>#Ошибка</a>
                                 <a className={"f-side-panel-button"} href={"#"}>#Другое</a>
                             </div>
