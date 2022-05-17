457a6fd84a7abcc144ff01a75f329c2e3c3e4e4a
Ильжан <ilzhan@ya.ru>
2022-05-17 08:21:57 +0500

    ci

--- patch.sh
+++
@@ -1,4 +0,0 @@
-cp patch/braft-finder/dist/index.js node_modules/braft-editor/node_modules/braft-finder/dist/
-cp patch/react-sortable-tree-patch-react-17/dist/index.esm.js node_modules/react-sortable-tree-patch-react-17/dist/
-cp patch/react-finder/es/* node_modules/react-finder/es/
-cp patch/react-finder/lib/* node_modules/react-finder/lib/
