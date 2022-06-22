3add144e8e16c208255af0ca705cbb93f1f36ecf
Sergey <jaxi@atma.company>
2022-06-22 16:01:50 +0500

    fix

--- src/React/Main/Auth/Login.js
+++ src/React/Main/Auth/Login.js
@@ -155,7 +155,7 @@ const Login = ({visible, setVisible}) => {
                                         style={{
                                             paddingRight: 27,
                                             paddingLeft: 27,
-                                            boxShadow: blockButton ? '0px 16px 32px 4px rgba(61, 114, 237, 0.24)' : 'none',
+                                            boxShadow: blockButton ? 'none' : '0px 16px 32px 4px rgba(61, 114, 237, 0.24)',
                                             borderRadius: 64,
                                             fontSize: 20,
                                             height: 60,
