53dbb3720261e244b1f469f417c29d9e02a57e01
Bob <bobcatoshigu@gmail.com>
2022-07-19 15:16:40 +0500

    fix routes

--- config/routes_react_guest.js
+++ config/routes_react_guest.js
@@ -2,13 +2,11 @@ import {default as MainPage} from "../src/React/MainPage";
 import {default as ShowIdea} from "../src/React/Main/Idea/ShowIdea";
 import {default as Profile} from "../src/React/Main/Profile/Profile";
 import {default as Redirect} from "../src/React/Main/RedirectPage/Redirect";
-import Settings from "../src/React/Main/Profile/Settings";
 
 export default [
     { path: "/", name: "Главная страница", Component: MainPage },
     { path: "/profile/:id/", name: "Профиль", Component: Profile},
     { path: "/idea/:id/", name: "Идея", Component: ShowIdea},
-    { path: "/user/settings/", name: "Настройки пользователя", Component: Settings },
     { path: "/redirect", name: "Редирект", Component: Redirect }
 ];
 
