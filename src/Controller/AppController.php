5719536ad6490244f1d029c9417e1674577486b0
Sergey <jaxi@atma.company>
2022-09-06 19:13:36 +0500

    fix controller

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -726,7 +726,20 @@ class AppController extends AbstractController
 
     static public function sendTelegramNotification($register_key, $notify_message)
     {
-        AppController::curl("https://rb19x.atma.company/atmaguru-notify/$register_key/?text=$notify_message", "POST");
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://rb19x.atma.company/atmaguru-notify/$register_key/",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => ["text" => $notify_message],
+        ));
+        $response = curl_exec($curl);
+
+        curl_close($curl);
     }
 
     static public function array_sort($array, $on, $order = SORT_ASC)
